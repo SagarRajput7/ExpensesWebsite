32a0999df87764cd994e12779ac120f37664054c
Sagar Rajput <43861831+Shaanrj@users.noreply.github.com>
2020-12-27 20:14:31 +0530

    Update settings.py

--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -75,28 +75,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'expensewebsite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'incomeexpensesdb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'sagar',
-#         'HOST': 'localhost',
-#     }
-# }
+Database
+https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': 'incomeexpensesdb',
+        'USER': 'postgres',
+        'PASSWORD': 'sagar',
+        'HOST': 'localhost',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#     }
+# }
 
 
 # Password validation
