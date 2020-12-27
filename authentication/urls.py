1d4f3d43ad1fc855e58641ddb299a4857849b09c
Sagar Rajput <43861831+Shaanrj@users.noreply.github.com>
2020-12-27 19:32:46 +0530

    Add files via upload

---
+++ authentication/urls.py
@@ -0,0 +1,23 @@
+from .views import RegistrationView,UsernameValidationView,RequestPasswordReset,CompletePasswordReset, EmailValidationView, LogoutView, VerificationView, LoginView
+from django.urls import path
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+urlpatterns = [
+    path('register', RegistrationView.as_view(), name="register"),
+    path('login', LoginView.as_view(), name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()),
+         name="validate-username"),
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()),
+         name='validate_email'),
+    path('activate/<uidb64>/<token>',
+         VerificationView.as_view(), name='activate'),
+    path('request-reset-link',RequestPasswordReset.as_view(),
+          name="request-password"),
+     path('set-new-password/<uidb64>/<token>',
+         CompletePasswordReset.as_view(), name="reset-user-password"),
+
+     
+]
---
+++ expenses/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path,include
+from . import views
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns=[
+    path('',views.index,name="expenses"),
+    path('authentication/',include('authentication.urls')),
+    path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
+    path('add-expenses',views.add_expenses,name="add-expenses"),
+    path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
+    path('search-expenses', csrf_exempt(views.search_expenses),
+         name="search_expenses"),
+    path('expense_category_summary',views.expense_category_summary,name="expense_category_summary"),
+    path('stats',views.stats_view,name="stats"),
+    path('export_csv',views.export_csv,name="export-csv"),
+    
+
+]
